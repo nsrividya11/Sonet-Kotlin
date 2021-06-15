960d11b0a64de3e8cc017d87ae9a9f13e6f483c1
nsrividya2601 <nsrividya26@gmail.com>
2021-06-15 14:16:05 +0530

    operators

--- app/src/main/java/com/example/kotlindemo/main.kt
+++ app/src/main/java/com/example/kotlindemo/main.kt
@@ -1,9 +1,51 @@
 package com.example.kotlindemo
 
 fun main() {
-    var name : String = "Android Kotlin"
-    var MyFloat : Float=13.37F
-    val pi : Double = 3.14159265358979
-    var MyInt : Int = 25
-    print(" String: $name \n Float: $MyFloat \n Pi: $pi \n Integer: $MyInt ")
+    /*arithmetic operators*/
+    val n1 = 10.8
+    val n2 = 16.5
+    var result : Double
+
+    result = n1 + n2
+    println("addition : $result")
+    result = n1 - n2
+    println("subtraction : $result")
+    result = n1 * n2
+    println("multiplication : $result")
+    result = n1/n2
+    println("quotient : $result")
+    result = n1%n2
+    println("remainder : $result")
+
+    /*assignment operator*/
+    var num = 15
+    num *= 2
+    println("number = $num")
+
+    /*comparison*/
+    val a = 20
+    val b = -5
+    val c = 10
+    println(a>b)
+    var res : Boolean
+    res = (a>b)&&(c<b)     //logical operator
+    println("$res")
+
+    /* in operator*/
+    val numbers = 1..20
+    if(7 in numbers) {
+        println("numbers array contains 7")
+    }
+
+    /* as operator*/
+    val s1:Any=7
+    val s2:String?= s1 as? String
+    println(s2)
+
+
+
+
+
+
+
 }
