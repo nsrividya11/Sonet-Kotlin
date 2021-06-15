558710620e6ae9bd5312722a096655281b081f1c
nsrividya2601 <nsrividya26@gmail.com>
2021-06-15 10:10:51 +0530

    variables and datatypes

---
+++ app/src/main/java/com/example/kotlindemo/main.kt
@@ -0,0 +1,9 @@
+package com.example.kotlindemo
+
+fun main() {
+    var name : String = "Android Kotlin"
+    var MyFloat : Float=13.37F
+    val pi : Double = 3.14159265358979
+    var MyInt : Int = 25
+    print(" String: $name \n Float: $MyFloat \n Pi: $pi \n Integer: $MyInt ")
+}
